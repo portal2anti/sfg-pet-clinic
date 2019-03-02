6697942b57f85460770130c852518c675d9c2a08
nikola <shu.xuan.shu@gmail.com>
2019-03-02 22:19:14 +0100

    Add interfaces for services,  closes #5

---
+++ pet-clinic-data/src/main/java/com/testhumans/sfgpetclinic/service/VetService.java
@@ -0,0 +1,15 @@
+package com.testhumans.sfgpetclinic.service;
+
+import com.testhumans.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
